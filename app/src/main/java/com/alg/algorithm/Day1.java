d9d1d5fa32bfc8d799d163e4d7559553cbf13403
유저이름 <>
2022-07-12 20:26:40 +0900

    backup

--- app/src/main/java/com/alg/algorithm/Day1.java
+++ app/src/main/java/com/alg/algorithm/Day1.java
@@ -1,12 +1,21 @@
+/*
+ * 2022-07-12(Tue)
+ * baokjun9498
+ * 문제: 시험 점수를 입력받아 90~100점은 A, 80~89점은 B, 70~79점은C, 60~69점은 D, 나머지 점수는 F를 출력하는 프로그램을 작성하시오.
+ * 입력: 첫째 줄에 시험 점수가 주어진다. 시험 점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다.
+ * 출력: 시험 성적을 출력한다.
+ */
 package com.alg.algorithm;
 
-public class Baekjoon9498 {
+public class Day1 {
 
   public static void main(String[] args) {
 
+    System.out.println("시험 점수를 입력하세요.");
 
     java.util.Scanner gradeInput = new java.util.Scanner(System.in);
     int grade = gradeInput.nextInt();
+    
     if (0 <= grade && grade <= 100) {
       if (grade >= 90) {
         System.out.println("A");
@@ -19,6 +28,8 @@ public class Baekjoon9498 {
       } else {
         System.out.println("F");
       }
+    } else {
+      System.out.println("0~100사이의 값을 입력해주세요.");
     }
   }
 }
