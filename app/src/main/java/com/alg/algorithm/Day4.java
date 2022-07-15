21a7be197011729def16c705bbd6eefacd5ff1b6
유저이름 <>
2022-07-15 19:51:10 +0900

    backup

---
+++ app/src/main/java/com/alg/algorithm/Day4.java
@@ -0,0 +1,35 @@
+/*
+ * 2022-07-15(Fri)
+ * baokjun10171
+ * 문제: 아래 예제와 같이 고양이를 출력하시오.
+ * 입력: 없음
+ * 출력: 고양이를 출력한다.
+ */
+package com.alg.algorithm;
+
+public class Day4 {
+
+  public static void main(String[] args) {
+    System.out.println("\\    /\\");
+    System.out.println(" )  ( ')");
+    System.out.println("(  /  )");
+    System.out.println("\\(__)|");
+  }
+}
+/*
+ * 2022-07-15(Fri)
+ * baokjun10171
+ * 문제: 아래 예제와 같이 개를 출력하시오.
+ * 입력: 없음
+ * 출력: 개를 출력한다.
+ */
+
+public class Day4 {
+  public static void main(String[] args) {
+    System.out.println("|\\\\_/|");
+    System.out.println("|q p|   /}");
+    System.out.println("( 0 )\"\"\"\\");
+    System.out.println("|\"^\"`    |");
+    System.out.println("||_/=\\\\__|");
+  }
+}
