fb495b3993eecffcc621c568811dbc042e2fa163
Achiiun <rlduwl0404@gmail.com>
2022-08-06 19:55:30 +0900

    Algorithm Day 25 Study

---
+++ app/src/main/java/com/alg/algorithm/Day25.java
@@ -0,0 +1,40 @@
+// copyright by codefights.com
+// 
+// 더하기 연산자를 이용하여 사각형의 면적을 구하라!
+// 예)
+//    rectangleArea(2, 3) = 6
+// 
+/*
+Find the area of a rectangle with given sides.
+
+Example
+
+rectangleArea(2, 3) = 6
+
+[input] integer a
+positive integer
+
+[input] integer b
+positive integer
+
+[output] integer
+ */
+//
+// [시간 복잡도]
+// - O(n) : n은 너비 또는 높이이다. 
+//
+public class Day25 {
+
+  public static void main(String[] args) {
+    System.out.println(rectangleArea(5, 9) == 45);
+    System.out.println(rectangleArea(3, 2) == 6);
+  }
+
+  static int rectangleArea(int a, int b) {
+    int result = 0;
+    for (int i = 0; i < a; i++) {
+      result += b;
+    }
+    return result;
+  }
+}
