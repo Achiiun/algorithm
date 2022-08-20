77d1cba273b1f4dcf6186424e4cf1136318eb092
Achiiun <rlduwl0404@gmail.com>
2022-08-20 10:07:58 +0900

    Morning Algorithm Day39 Study

---
+++ app/src/main/java/com/alg/algorithm/Day39.java
@@ -0,0 +1,22 @@
+package com.alg.algorithm;
+
+/*
+ * 2022-08-20(Sat)
+ * baokjun10171
+ * 문제: 아래 예제와 같이 새싹을 출력하시오.
+ * 입력: 없음.
+ * 출력: 새싹을 출력한다.
+ */
+
+
+
+public class Day39 {
+    public static void main(String[] args) {
+      System.out.println("         ,r'\"7");
+      System.out.println("r`-_   ,'  ,/");
+      System.out.println(" \\. \". L_r'");
+      System.out.println("   `~\\/");
+      System.out.println("      |");
+      System.out.println("      |");
+    }
+  }
