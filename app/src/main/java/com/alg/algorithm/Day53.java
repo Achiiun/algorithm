dc44aac15526f0c55308df425e00841229e2f19f
Achiiun <rlduwl0404@gmail.com>
2022-09-03 14:22:11 +0900

    Algorithm Day 53 study

---
+++ app/src/main/java/com/alg/algorithm/Day53.java
@@ -0,0 +1,40 @@
+package com.alg.algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+ * 2022-09-03(Sat)
+ * baokjun10952
+ * 문제: 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+ * 입력: 입력은 여러 개의 테스트 케이스로 이루어져 있다.
+ *      각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+ *      입력의 마지막에는 0 두 개가 들어온다.
+ * 출력: 각 테스트 케이스마다 A+B를 출력한다.
+ */
+
+
+public class Day53 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    StringTokenizer st;
+
+    while (true) {
+        st = new StringTokenizer(br.readLine(), " ");
+        
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        if (a == 0 && b == 0) {
+            br.close();
+            break;
+        }
+        System.out.println(a + b);
+    }
+    br.close();
+
+  }    
+}
