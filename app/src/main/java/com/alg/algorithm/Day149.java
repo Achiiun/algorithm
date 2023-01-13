cc399379db16c95ecd28e20f2952e0ddb99cb9b9
Achiiun <rlduwl0404@gmail.com>
2023-01-13 20:14:52 +0900

    Algorithm Day 150 Study

--- app/src/main/java/com/alg/algorithm/Day149.java
+++ app/src/main/java/com/alg/algorithm/Day149.java
@@ -3,6 +3,8 @@ package com.alg.algorithm;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 /*
  * 2023-01-11(Wed)
  * baokjun1629: 곱셈
@@ -10,7 +12,7 @@ import java.io.InputStreamReader;
  * 입력: 첫째 줄에 A, B, C가 빈 칸을 사이에 두고 순서대로 주어진다. A, B, C는 모두 2,147,483,647 이하의 자연수이다.
  * 출력: 첫째 줄에 A를 B번 곱한 수를 C로 나눈 나머지를 출력한다.
  */
-import java.util.StringTokenizer;
+
 public class Day149 {
 
   public static long C;
